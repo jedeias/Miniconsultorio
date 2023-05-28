6dbf0420b2628423d487a496cbef657317fba88f
DevAlexBreno <alex.breno003@gmail.com>
2023-05-27 21:33:21 -0300

    Create a classes people, patient and psychologist

---
+++ app/model/Patient.php
@@ -0,0 +1,21 @@
+<?php 
+
+Class Patient extends People{
+    private Psychologist $psychologist;
+
+    public function __construct($psychologist){
+        $this->psychologist = $psychologist;
+    }
+
+	public function getPsychologist(): Psychologist {
+		return $this->psychologist;
+	}
+
+	public function setPsychologist(Psychologist $psychologist): self {
+		$this->psychologist = $psychologist;
+		return $this;
+	}
+}
+
+
+?>
